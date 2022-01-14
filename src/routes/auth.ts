510a6be92f2678bb594fd24bd1c085c8c35a18d2
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-14 16:17:13 -0300

    add login feature with JWT and add encrypt password at sign up

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,22 +1,34 @@
 import { Router } from "express";
 import { check } from "express-validator";
 
-import { signUp } from "../controllers/auth";
+import { signUp, logIn } from "../controllers/auth";
 import validateFields from "../middlewares/validateFields";
 import { userExists } from "../middlewares/dbValidators";
 
 const router = Router();
 
 router.post(
-    "/signup",
-    [
-        check("name", "Name is required").not().isEmpty(),
-        check("email", "Email is required").isEmail(),
-        check("password", "Password is required - Min length is 8").isLength({ min: 8 }),
-        userExists,
-        validateFields
-    ],
-    signUp
+  "/signup",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required - Min length is 8").isLength({
+      min: 8,
+    }),
+    userExists,
+    validateFields,
+  ],
+  signUp
+);
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+  logIn
 );
 
 export default router;
