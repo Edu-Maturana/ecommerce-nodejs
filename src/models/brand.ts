ba37a8080407edc41974d1fbd6f26b7f5319211e
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-13 22:53:45 -0300

    a

--- src/models/brand.ts
+++ src/models/brand.ts
@@ -4,28 +4,28 @@ import { nanoid } from "nanoid";
 import connection from "../../database/connection";
 
 class Brand extends Model {
-    public id!: string;
-    public name!: string;
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+  public id!: string;
+  public name!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 Brand.init(
-    {
-        id: {
-            type: DataTypes.STRING,
-            primaryKey: true,
-            defaultValue: nanoid(),
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      defaultValue: nanoid(),
     },
-    {
-        tableName: "brands",
-        sequelize: connection,
-    }
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "brands",
+    sequelize: connection,
+  }
 );
 
 export default Brand;
