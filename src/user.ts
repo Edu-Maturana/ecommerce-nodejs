b217ea0a5368dedce588e3d53a7e80fd7a964f17
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-01-13 21:56:35 -0300

    create user model

---
+++ src/user.ts
@@ -0,0 +1,47 @@
+import { Model, Sequelize, DataTypes } from "sequelize";
+import {nanoid} from 'nanoid';
+
+import connection from "../database/connection";
+
+class User extends Model {
+  public id!: number;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public address!: string;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            defaultValue: nanoid(),
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "users",
+        sequelize: connection,
+    }
+);
+
+export default User;
